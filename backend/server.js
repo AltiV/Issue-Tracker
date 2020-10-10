7d2a0f26166864704219ba07a53d57aae28709d9
AltiV <vuong.alan.t@gmail.com>
2020-10-09 23:17:19 -0400

    server.js update again

--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get("*", (request, response) => {
-  response.sendFile(path.join(__dirname, "client/build", "index.html"));
+app.use(express.static(path.join(__dirname, "../build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build"));
 });
 
 const uri = process.env.ATLAS_URI;
