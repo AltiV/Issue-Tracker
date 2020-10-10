ef8eeb0171682d39167da317acfc89b6ae22d03e
AltiV <vuong.alan.t@gmail.com>
2020-10-09 23:07:12 -0400

    server.js test

--- backend/server.js
+++ backend/server.js
@@ -12,15 +12,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+app.get("*", (request, response) => {
+  response.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {
